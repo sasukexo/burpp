e67a1cfea73fa9676eb0928ac70729bfcdc323aa
Sasukexo <aswinash1002@gmail.com>
2023-09-17 21:57:42 +0530

    Vainkoo

--- dk/hello/urls.py
+++ dk/hello/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
-    path('v1/',views.v1,name='index')
+     
     
 ]
--- dk/hello/views.py
+++ dk/hello/views.py
@@ -5,5 +5,3 @@ from django.http import HttpResponse
 def index(response):
     return HttpResponse("<h1>Maja maamey</h1>")
 
-def v1(response):
-    return HttpResponse("<h1>view 1</h1>")
