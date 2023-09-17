9a407e7ee1a32c3871ad3dbc77ce21b8a1b0b8d6
Sasukexo <aswinash1002@gmail.com>
2023-09-17 22:00:24 +0530

    frdh

--- dk/hello/views.py
+++ dk/hello/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Maja maamey</h1>")
+def index(response,id):
+    return HttpResponse("<h1>%s</h1>  "%id)
 
