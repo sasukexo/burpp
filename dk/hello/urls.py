e23cffa2fb3693de9aa21e4954f2940606017020
Sasukexo <aswinash1002@gmail.com>
2023-09-17 22:13:37 +0530

    nice

--- dk/hello/urls.py
+++ dk/hello/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('<int:id>',views.index,name='index'),
+    path('<str:name>',views.index,name='index'),
      
     
 ]
--- dk/hello/views.py
+++ dk/hello/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
-def index(response,id):
-    ls=ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>  "%ls.name)
+def index(response,name):
+    ls=ToDoList.objects.get(name=name)
+    item=ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1>  <br><br><p>%s</p>"%(ls.name,str(item.text)))
 
